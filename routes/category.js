d80187646079dceb3019b3cc4a62eab94ce17d60
khizersh <moh.khizer61@gmail.com>
2023-03-26 16:17:38 +0500

    slug wala kam

--- routes/category.js
+++ routes/category.js
@@ -19,9 +19,9 @@ router.get("/get-category", async (req, res) => {
 router.post("/get-product-by-category", async (req, res) => {
   let request = req.body;
   try {
-    const section = await Category.findById({ _id: request.categoryId }).populate(
-      "products"
-    );
+    const section = await Category.findById({
+      _id: request.categoryId,
+    }).populate("products");
 
     res.send({ status: 0000, message: "success", data: section }).status(200);
   } catch (error) {
@@ -29,6 +29,21 @@ router.post("/get-product-by-category", async (req, res) => {
     res.send({ status: 9999, message: "Something went wrong!" }).status(200);
   }
 });
+
+router.post("/get-by-slug", async (req, res) => {
+  let request = req.body;
+  try {
+    const section = await Category.find({
+      categorySlug: request.categorySlug,
+    }).populate("products");
+
+    res.send({ status: 0000, message: "success", data: section }).status(200);
+  } catch (error) {
+    console.log("error : ", error);
+    res.send({ status: 9999, message: "Something went wrong!" }).status(200);
+  }
+});
+
 router.get("/get-category-products", async (req, res) => {
   try {
     const section = await Category.find({}).populate("products");
@@ -44,10 +59,7 @@ router.post("/add-category", async (req, res) => {
   const request = new Category(req.body);
 
   try {
-
     if (request.categoryTitle) {
- 
-
       let slug = convertToSlug(request.categoryTitle);
       let isExist = await Category.exists({ categorySlug: slug });
       if (isExist != null) {
@@ -57,6 +69,36 @@ router.post("/add-category", async (req, res) => {
     }
     const category = await request.save();
     res.send({ status: 0000, message: "success", data: category }).status(200);
+  } catch (error) {
+    console.log("error : ", error);
+    res.send({ status: 9999, message: error.message }).status(200);
+  }
+});
+router.post("/add-product-to-category", async (req, res) => {
+  // const request = new Category(req.body);
+  const request = req.body;
+
+  try {
+    if (request.categorySlug && request.productList) {
+      const category = await Category.findOne({
+        categorySlug: request.categorySlug,
+      });
+      console.log("request : ", request, category);
+      if (category) {
+        let products = [];
+        products = category.products;
+        request.productList.map((m) => {
+          products.push(m);
+        });
+        category.products = products;
+        category.save();
+        res.send({ status: 0000, message: "success" }).status(200);
+      } else {
+        res.send({ status: 9999, message: "Invalid call!" }).status(200);
+      }
+    } else {
+      res.send({ status: 9999, message: "Invalid call!" }).status(200);
+    }
   } catch (error) {
     console.log("error : ", error);
     res.send({ status: 9999, message: "Something went wrong!" }).status(200);
