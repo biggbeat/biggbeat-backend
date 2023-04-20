4a475990f2ef060470653cf0e9d029a8aa21df6a
biggbeat <biggbeatx@gmail.com>
2023-04-20 19:40:25 +0500

    banner delete

--- routes/banner.js
+++ routes/banner.js
@@ -9,10 +9,14 @@ router.get("/get-banner", async (req, res) => {
   try {
     const banner = await Banner.find({});
 
-    res.send({ status: SUCCESS_CODE, message: "success", data: banner }).status(200);
+    res
+      .send({ status: SUCCESS_CODE, message: "success", data: banner })
+      .status(200);
   } catch (error) {
     console.log("error : ", error);
-    res.send({ status: ERROR_CODE, message: "Something went wrong!" }).status(200);
+    res
+      .send({ status: ERROR_CODE, message: "Something went wrong!" })
+      .status(200);
   }
 });
 
@@ -23,12 +27,32 @@ router.post("/add-banner", async (req, res) => {
   try {
     const banner = await request.save();
 
-    res.send({ status: SUCCESS_CODE, message: "success", data: banner }).status(200);
+    res
+      .send({ status: SUCCESS_CODE, message: "success", data: banner })
+      .status(200);
   } catch (error) {
     console.log("error : ", error);
-    res.send({ status: ERROR_CODE, message: "Something went wrong!" }).status(200);
+    res
+      .send({ status: ERROR_CODE, message: "Something went wrong!" })
+      .status(200);
   }
 });
+router.delete("/delete-banner/:id", async (req, res) => {
+  //   // let path = "../backend/csv/user.csv";
 
+  try {
+    if (req.params.id) {
+      await Banner.deleteOne({ _id: req.params.id });
+      res.send({ status: SUCCESS_CODE, message: "success" }).status(200);
+    } else {
+      res.send({ status: ERROR_CODE, message: "Invalid cal!" }).status(200);
+    }
+  } catch (error) {
+    console.log("error : ", error);
+    res
+      .send({ status: ERROR_CODE, message: "Something went wrong!" })
+      .status(200);
+  }
+});
 
 module.exports = router;
