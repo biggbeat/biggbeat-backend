343750fca10a70883ef630ca6ac5e4ab286c0be3
biggbeat <biggbeatx@gmail.com>
2023-05-17 17:02:32 +0500

    location api

--- routes/location.js
+++ routes/location.js
@@ -5,33 +5,13 @@ const router = express.Router();
 
 router.get("/getAllCountries", async (req, res) => {
   try {
-    const path = "json/location.json";
+    const path = "json/country.json";
 
     fs.readFile(path)
       .then((fileData) => {
         const json = JSON.parse(fileData);
-        const countries = Object.keys(json);
-        
-      //   let result = countries.map((m) => {
-      //     return {
-      //       country: m,
-      //       shipment: 150,
-      //       active: true,
-      //     };
-      //   });
-
-      //   const jsonString = JSON.stringify(result);
-
-      //   fs.writeFile('json/country.json', jsonString, err => {
-      //     if (err) {
-      //         console.log('Error writing file', err)
-      //     } else {
-      //         console.log('Successfully wrote file')
-      //     }
-      // })
-
         res
-          .send({ status: SUCCESS_CODE, message: "success", data: countries })
+          .send({ status: SUCCESS_CODE, message: "success", data: json })
           .status(200);
       })
       .catch((error) => {
@@ -39,12 +19,38 @@ router.get("/getAllCountries", async (req, res) => {
         res
           .send({ status: ERROR_CODE, message: "Something went wrong!" })
           .status(200);
-        // Do something if error
       });
+  } catch (error) {
+    console.log("error : ", error);
+    res
+      .send({ status: ERROR_CODE, message: "Something went wrong!" })
+      .status(200);
+  }
+});
 
-    // res
-    // .send({ status: SUCCESS_CODE, message: "success", data: "asfasfa" })
-    // .status(200);
+
+router.post("/getCitiesByCountry", async (req, res) => {
+  try {
+    const {country} = req.body;
+    const path = "json/location.json";
+
+    fs.readFile(path)
+      .then((fileData) => {
+        const json = JSON.parse(fileData);
+        var data = json[country];
+        if(!data){
+          data = []
+        }
+        res
+          .send({ status: SUCCESS_CODE, message: "success", data: data })
+          .status(200);
+      })
+      .catch((error) => {
+        console.log("error : ", error);
+        res
+          .send({ status: ERROR_CODE, message: "Something went wrong!" })
+          .status(200);
+      });
   } catch (error) {
     console.log("error : ", error);
     res
